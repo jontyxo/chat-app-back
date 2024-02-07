1be4f15edd3be992e923c300a3086b6f35451ec2
Joyesh Wadhwa <65168230+jontyxo@users.noreply.github.com>
2024-02-07 22:13:09 +0530

    added /server path

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const http=require('http');
 const cors=require('cors')
 const {Server} = require('socket.io')
 
+
+
+
 app.use(cors());
+
 
 const server= http.createServer(app);
 // const allowedOrigins=['http://localhost:3000/','https://chat-lemon-gamma.vercel.app']
@@ -14,21 +18,31 @@ const io=new Server(server,{
     }
 });
 
+
 io.on("connection",(socket)=>{
 console.log(socket.id);
 socket.on("join_room",(data)=>{
   socket.join(data);
   console.log(`User  with id ${socket.id} joined the room ${data}`);
-      
+     
 })
 socket.on("send_message",(data)=>{
     socket.to(data.room).emit("recieve_message",data)})
+
+
 
 
 socket.on("disconnect",()=>{
     console.log("User disconnected: " + socket.id);
 })
 })
+
+
+// Route for "/server" path
+app.get('/server', (req, res) => {
+    res.send('Success');
+});
+
 
 server.listen(3001,()=>{
     console.log("Server started on 3001");
